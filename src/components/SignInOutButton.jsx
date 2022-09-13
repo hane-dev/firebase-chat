9de5f8d76d4b5b7a78783f111111a49837ce12d3
hane-dev <hannu.hoglund@gmail.com>
2022-09-13 09:39:17 +0300

    hello

--- src/components/SignInOutButton.jsx
+++ src/components/SignInOutButton.jsx
@@ -1,12 +1,13 @@
 import { useAuth, useSigninCheck } from "reactfire";
 import { signInWithGoogle, signOut } from "../utils/firebase/auth";
+import { Button } from "@mui/material";
 
 export default function SignInOutButton() {
   const { data: signInCheckResult } = useSigninCheck();
   const auth = useAuth();
 
   return signInCheckResult.signedIn ? (
-    <button onClick={() => signOut(auth)}>Sign out</button>
+    <Button variant="contained" onClick={() => signOut(auth)}>Sign out</Button>
   ) : (
     <button onClick={() => signInWithGoogle(auth)}>Sign in with Google</button>
   );
